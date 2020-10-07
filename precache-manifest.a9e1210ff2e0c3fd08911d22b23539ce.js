646b8abe0a6cba079e9e2aaa3f0b901e2b9ea66d
Thiimo <thimosietsma@gmail.com>
2020-10-07 21:04:15 +0200

    Updates

---
+++ precache-manifest.a9e1210ff2e0c3fd08911d22b23539ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfda94c6ccc14dfc82088b775f381fab",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f6a6cfe3b28f7dab2a5c",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/test/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a6cfe3b28f7dab2a5c",
+    "url": "/test/static/js/main.f86fb908.chunk.js"
+  },
+  {
+    "revision": "0d6dc1693978d1d8b578",
+    "url": "/test/static/js/runtime-main.cce556a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
